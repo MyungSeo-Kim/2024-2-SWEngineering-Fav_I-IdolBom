4cada8c3cde313fd8d37e5ab039d7d79e83e2a19
geopark021 <geobrown021@gmail.com>
2024-11-11 04:33:53 +0900

    feat: 카카오 로그인 페이지 출력, 카카오 로그인 버튼 클릭 시 입력창 이동까지 구현

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,26 +1,19 @@
+// App.tsx
 import React from 'react';
-import styled from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/login/LoginScreen';
 
-
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #f5f5f5;
-`;
-
-const Title = styled.Text`
-  font-size: 24px;
-  color: #333;
-  font-weight: bold;
-`;
+const Stack = createStackNavigator();
 
 const App: React.FC = () => {
-  return (
-    <Container>
-      <Title>Hello, Styled Component</Title>
-    </Container>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Login" component={LoginScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default App;
--- Frontend/babel.config.js
+++ Frontend/babel.config.js
@@ -1,3 +1,17 @@
+// babel.config.js
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        allowUndefined: true,
+      },
+    ],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+  ],
 };
